95524d5ece04eb132aaa8b28d2169e9cc552bbbc
Andrew Hennessy <andrew@andrewhennessy.net>
2018-12-04 16:55:57 -0500

    Fix hw7 commit

--- hw7/src/LinkedQueue.java
+++ hw7/src/LinkedQueue.java
@@ -2,48 +2,214 @@ import javafx.collections.ModifiableObservableListBase;
 import java.util.NoSuchElementException;
 import java.util.Iterator;
 
-public class LinkedQueue<E> extends ModifiableObservableListBase implements Iterable, SimpleQueue {
+/**
+ * @author ahennessy6
+ * @version 1.0.1
+ * @param <E> is the generic type to be later defined defines the generic types
+ *        in nodes
+ */
+public class LinkedQueue<E> extends ModifiableObservableListBase<E>
+        implements Iterable<E>, SimpleQueue<E> {
+    private LinkedQueueNode<E> head;
+    private LinkedQueueNode<E> tail;
+    private int n;
 
-    @Override
-    public void enqueue(Object element) {
-        // TODO Auto-generated method stub
-        
+
+    /**
+     * LinkedQueue Constructor.
+     */
+    public LinkedQueue() {
+        this.head = null;
+        this.tail = null;
+        this.n = 0;
+    }
+
+    /**
+     * @return an iterator
+     */
+    public Iterator<E> iterator() {
+        return new QueueIterator();
+    }
+
+    /**
+     * @author andrewhennessy
+     * QueueIterator Private Inner Class
+     */
+    private class QueueIterator implements Iterator<E> {
+        private LinkedQueueNode<E> current = head;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            E e = current.getData();
+            current = current.getNext();
+            return e;
+        }
+
+    }
+
+    /**
+     * lets us know if linked list is empty
+     * @return if the linked list is empty as boolean
+     */
+    public boolean isEmpty() {
+        return head == null;
     }
 
     @Override
-    public Object dequeue() {
-        // TODO Auto-generated method stub
-        return null;
+    @SuppressWarnings("unchecked")
+    public void enqueue(E element) {
+        super.add(element);
+    }
+
+    /**
+     * @param element that should be passed in.
+     */
+    public void enqueueTwo(E element) {
+        LinkedQueueNode<E> previousLast = tail;
+        tail = new LinkedQueueNode<E>((E) element);
+        if (isEmpty()) {
+            head = tail;
+        } else {
+            previousLast.setNext(tail);
+        }
+        n++;
+
     }
 
     @Override
-    public Object get(int index) {
-        // TODO Auto-generated method stub
-        return null;
+    public E dequeue() {
+        return super.remove(0);
+    }
+
+    /**
+     * @return E from dequeueTwo using nodes
+     */
+    public E dequeueTwo() {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException("");
+        }
+        E fromFirst = head.getData();
+        head = head.getNext();
+        n--;
+        if (this.isEmpty()) {
+            tail = null;
+        }
+        return fromFirst;
+    }
+
+    @Override
+    public E get(int index) {
+        Iterator<E> queueIterator = this.iterator();
+
+        int counter = 0;
+        E buffer = null;
+        E finalBuffer = null;
+        if (index > this.size() - 1) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            while (queueIterator.hasNext()) {
+                if (counter == index) {
+                    finalBuffer = queueIterator.next();
+                    return finalBuffer;
+                } else {
+                    buffer = queueIterator.next();
+                    counter++;
+                }
+            }
+            return finalBuffer;
+        }
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+        return n;
+    }
+
+    /**
+     * clears
+     */
+    public void clear() {
+        n = 0;
+        head = null;
+        tail = null;
     }
 
     @Override
-    protected void doAdd(int index, Object element) {
-        // TODO Auto-generated method stub
-        
+    protected void doAdd(int index, E element) {
+        // System.out.println(index);
+        if (index == 0) {
+            this.enqueueTwo(element);
+        } else {
+            // watch out for -1
+            if (index > this.size()) {
+                throw new IndexOutOfBoundsException();
+            } else {
+                for (int i = 0; i < index; i++) {
+                    this.enqueueTwo(this.dequeueTwo());
+                }
+                this.enqueueTwo(element);
+                for (int i = index + 1; i < this.size(); i++) {
+                    this.enqueueTwo(this.dequeueTwo());
+                }
+            }
+        }
     }
 
     @Override
-    protected Object doSet(int index, Object element) {
-        // TODO Auto-generated method stub
-        return null;
+    protected E doSet(int index, E element) {
+        if (1 == 1) {
+            throw new UnsupportedOperationException();
+        } else {
+            return element;
+        }
+        /*E buffer = null;
+        // watch out for -1
+        if (index > this.size() - 1) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            buffer = this.get(index);
+            for (int i = 0; i < index; i++) {
+                this.enqueueTwo(this.dequeueTwo());
+            }
+            buffer = this.dequeueTwo();
+            this.enqueueTwo(element);
+            for (int i = index + 1; i < this.size(); i++) {
+                this.enqueueTwo(this.dequeueTwo());
+            }
+
+        }
+        return buffer;
+        */
     }
 
     @Override
-    protected Object doRemove(int index) {
-        // TODO Auto-generated method stub
-        return null;
+    protected E doRemove(int index) {
+        E buffer = null;
+        // watch out for -1
+        if (index > this.size()) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            if (this.size() == 0 && index == 0) {
+                return null;
+            }
+            buffer = this.get(index);
+            for (int i = 0; i < index; i++) {
+                this.enqueueTwo(this.dequeueTwo());
+            }
+            this.dequeueTwo();
+            for (int i = index; i < this.size(); i++) {
+                this.enqueueTwo(this.dequeueTwo());
+            }
+        }
+        return buffer;
     }
 
 }
--- hw7/src/OfficeHourQueue.java
+++ hw7/src/OfficeHourQueue.java
@@ -1,31 +1,96 @@
+import java.util.Optional;
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.beans.binding.Bindings;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class OfficeHourQueue extends Application{
+/**
+ * @author ahennessy6
+ * @version 1.0.1
+ */
+public class OfficeHourQueue extends Application {
 
-    public static void main(String[] args) {
-        //start(null);
-        //Scene scene = new Scene();
-        //state.setScene(scene);
-        //stage.setTitle("Todos");
-        //stage.show();
-
-    }
+    private LinkedQueue<String> queue;
+    private ListView<String> listView;
+    private int maxQueue = 0;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Button addButton = new Button();
-        addButton.setText("Add");
-        
-        //VBox vbox = new VBox();
-        //vbox.getChildren().addAll(elements)
-        
-        
-    }
+    public void start(Stage stage) {
+        queue = new LinkedQueue<String>();
 
+        Label currentLabel = new Label(
+                "Current Number Of Students In Queue: 0");
+        Label maxLabel = new Label("Max Number of Students in Queue: 0");
+
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle("Privilege Check");
+        dialog.setHeaderText("Confirmation");
+        dialog.setContentText("Please Enter Password:");
+
+        listView = new ListView<String>(queue);
+
+        Button addButton = new Button();
+        addButton.setText("Add Student");
+
+        Button removeButton = new Button();
+        removeButton.setText("Remove Student");
+
+        TextField inputField = new TextField();
+
+        addButton.setOnAction(e -> {
+                queue.enqueue(inputField.getText());
+            // System.out.println(queue);
+                currentLabel.setText(
+                    String.format("Current Number Of Students In Queue: %d",
+                            this.queue.size()));
+                this.maxQueue++;
+                maxLabel.setText(String.format(
+                    "Max Number Of Students In Queue: %d", this.maxQueue));
+
+                inputField.setText("");
+                inputField.requestFocus();
+            });
+
+        removeButton.setOnAction(e -> {
+                Optional<String> result = dialog.showAndWait();
+                if (result.isPresent()) {
+                    //System.out.println(result.get());
+                    if (result.get().equals((String) "CS1321R0X")) {
+                        queue.dequeue();
+                    // System.out.println(queue);
+                        currentLabel.setText(String.format(
+                              "Current Number Of Students In Queue: %d",
+                              this.queue.size()));
+                    } else {
+                    assert true;
+                    }
+                }
+
+                inputField.setText("");
+                inputField.requestFocus();
+            });
+
+        removeButton.disableProperty().bind(Bindings.isEmpty(queue));
+
+        addButton.disableProperty()
+                .bind(Bindings.isEmpty(inputField.textProperty()));
+
+        HBox entryBox = new HBox();
+        entryBox.getChildren().addAll(inputField, addButton, removeButton);
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(currentLabel, maxLabel, listView, entryBox);
+
+        Scene scene = new Scene(vbox);
+        stage.setScene(scene);
+        stage.setTitle("CS 1321 Office Hours Queue");
+        stage.show();
+    }
 }
